f55b02edacb16f08590f1cf90d65b26db7eb6f0f
Narongrit2544 <boomza532144@gmail.com>
2024-09-29 00:36:20 +0700

    2commit

--- unit_test.py
+++ unit_test.py
@@ -5,16 +5,16 @@ class PrimeTestCase(unittest.TestCase):
 
     def true_when_x_is_17(self):
 
-        response = app.show_number('/is_prime/17')
+        response = app.show_number(17)
         self.assertEqual(response, {'is_prime': True})
 
     def false_when_x_is_36(self):
         
-        response = app.show_number('/is_prime/36')
+        response = app.show_number(36)
         self.assertEqual(response, {'is_prime': False})
     def true_when_x_is_13219(self):
         
-        response = app.show_number('/is_prime/13219')
+        response = app.show_number(13219)
         self.assertEqual(response, {'is_prime': True})
 
 
