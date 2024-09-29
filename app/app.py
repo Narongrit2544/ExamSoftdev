51d48e5a468f9e31b7956ffa0035d47de5d81c83
Narongrit2544 <boomza532144@gmail.com>
2024-09-29 23:35:33 +0700

    commit

--- app/app.py
+++ app/app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 def index():
     return "ExamSoftdev Success!!!!!"
 
-@app.route('/is_prime/<number>',methods=['GET'])
-def show_number(number):
-    # แปลงค่าจาก string เป็น int
-    number = int(number)
+@app.route('/is_prime/<x>',methods=['GET'])
+def show_number(x):
 
-    if number <= 1:
+    x = int(x)
+
+    if x <= 1:
         return 'False'
     
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
             return 'False'
     
     return 'True'
